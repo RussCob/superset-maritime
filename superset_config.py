38d09aeb96aef24394c67fb701a4d90469c81cae
RussCob <resul.cobanoglu@outlook.com>
2025-05-23 00:24:39 -0400

    Connect Superset to Supabase DB

--- superset_config.py
+++ superset_config.py
@@ -1,7 +1,10 @@
 from superset.config import *
 SECRET_KEY = 'rRybq5bmB/Kn6NahcToikLms/+SP3DMWqMC1ENqDMqJx+xHzw7YRL9SX'
 AUTH_TYPE = AUTH_DB
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://supersetuser:supersetpass@localhost:5432/supersetdb'
+#ForLocal
+#SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://supersetuser:supersetpass@localhost:5432/supersetdb'
+#For Render supabase
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Admin1234@db.eeceixlcqpwrjdepspcn.supabase.co:5432/postgres'
 
 
 
