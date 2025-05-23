87a50eec2e29b87229c5eaea66ff0e901004f51a
RussCob <resul.cobanoglu@outlook.com>
2025-05-23 11:34:09 -0400

    Update DB connection and Flask app

--- superset_config.py
+++ superset_config.py
@@ -4,7 +4,7 @@ AUTH_TYPE = AUTH_DB
 #ForLocal
 #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://supersetuser:supersetpass@localhost:5432/supersetdb'
 #For Render supabase
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Admin1234@db.eeceixlcqpwrjdepspcn.supabase.co:5432/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.eeceixlcqpwrjdepspcn:Admin1234@aws-0-us-east-2.pooler.supabase.com:6543/postgres'
 
 
 
